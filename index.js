0eae4c5c4331f2d942dd08ebc93d4cf18b2947f1
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-09 02:04:50 -0500

    post json object from client side using object and postman working

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const app = express();
 const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 
+app.use(express.json());
+
 app.use(bodyParser.json());
 
 app.get("/users", (req, res) => res.send(state.users));
 
 app.get("/users/1", (req, res) => res.send(state.users[0]));
 
-app.post("/users", (req, res) => {
-  state.users.push({
-    _id: 6,
-    name: "TEST NAME",
-    occupation: "SPECIAL AGENT",
-    avatar: "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
-  });
-  res.json(state.users[state.users.length - 1]);
-});
+// app.post("/users", (req, res) => {
+//   state.users.push({
+//     _id: 6,
+//     name: "TEST NAME",
+//     occupation: "SPECIAL AGENT",
+//     avatar: "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+//   });
+//   res.json(state.users[state.users.length - 1]);
+// });
 
 app.put("/users/1", (req, res) => {
   state.users[0]["name"] = "TESTING";
@@ -30,6 +32,13 @@ app.delete("/users/1", (req, res) => {
   res.send("DELETED THE ITEM");
 });
 
-app.post("/users", (req, res) => {});
+app.post("/users", (req, res) => {
+  const user = {
+    id: state.users.length + 1,
+    name: req.body.name
+  };
+  state.users.push(user);
+  res.send(user);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
